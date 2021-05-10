2c59a10bfcef8cba6daefec2920a694c3a425322
ctrenkov <ctrenkov@gmail.com>
2021-05-10 01:22:12 -0400

    added glew, glm and glfw dependencies

--- DVZClient/src/main.cpp
+++ DVZClient/src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
-
+#include <glm/glm.hpp>
 #include "core/test.hpp"
+#include <gl/glew.h>
+#include <GLFW/glfw3.h>
 
 int main() {
+	glm::vec3 v{0, 2, 3};
 	std::cout << DVZ::add(10, 10);
 }
