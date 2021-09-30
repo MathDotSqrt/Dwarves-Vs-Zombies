e1f0c1d6d29bb3aa7b9d2828764a1009f5b50d18
ctrenkov <ctrenkov@gmail.com>
2021-09-29 22:03:05 -0400

    voxels now render

--- DVZClient/src/graphics/BasicRenderer.cpp
+++ DVZClient/src/graphics/BasicRenderer.cpp
@@ -83,7 +83,9 @@ void BasicRenderer::render(const InterpolatedScene& scene, const Voxel::ChunkRen
 		const Graphics::VAO& vao = chunk.getVAO();
 		const Graphics::VBO& ebo = chunk.getEBO();
 
-		chunk_shader->setUniform3f("u_pos", glm::vec3(coords));
+		glm::vec3 chunk_world_pos = glm::vec3(coords) * Voxel::BLOCK_WIDTH * glm::vec3(Voxel::CHUNK_X, Voxel::CHUNK_Y, Voxel::CHUNK_Z);
+
+		chunk_shader->setUniform3f("u_pos", chunk_world_pos);
 		chunk_shader->setUniformMat4("VP", VP);
 		vao.bind();
 		ebo.bind();
